e65d73400909cd093353012ee1da8545bcf3d0e9
pom <pom@poms-MBP.mshome.net>
2016-10-06 12:58:05 +0200

    Add more sizes (all the sizes!)

--- export_web.py
+++ export_web.py
@@ -4,14 +4,27 @@ import sys
 sys.path.append('/usr/share/inkscape/extensions')
 import inkex
 import os
+import operator
 import subprocess
 
+icon_sizes = { 
+        'android': [48*i for i in range(1,5)], 
+        'windows': [16,32,48,256],
+        'metro': [30,50,150],
+        'macos': [1024,512,256,128,64,32,16],
+        'gnome': [24,48,96],
+        'ios6': [57,114,1024,72,144,512],
+        'ios8': [120,180,1024,120,80, 152,76,58,29],
+        'windowsphone': [62,173,99,200],
+        }
+
+all_sizes = list(set(reduce(operator.add, icon_sizes.values())))
 
 class PNGExport(inkex.Effect):
     def __init__(self):
         """init the effetc library and get options from gui"""
         inkex.Effect.__init__(self)
-        self.OptionParser.add_option("--path", action="store", type="string", dest="path", default="~/webicons", help="")
+        self.OptionParser.add_option("--path", action="store", type="string", dest="path", default="./webicons", help="")
 
     def effect(self):
         dirname = os.path.expanduser(self.options.path)
@@ -24,16 +37,15 @@ class PNGExport(inkex.Effect):
 
         curfile = self.args[-1]
 
-        for fs in [(92,92), (152,152),(48,48)]:
+        for fs in all_sizes:
             self.exportToPng(curfile, dirname, fs)
 
 
     def exportToPng(self, svg_path, dirname, size):
-        w, h = size
-        output_filename = os.path.join(dirname, "icon-{}x{}.png".format(w,h))
-        command = "inkscape -C -w {w} -h {h} -e \"{}\" \"{}\"".format(output_filename, svg_path, w=w, h=h)
-        print(command)
-        inkex.errormsg(command)
+        filename = "icon-{}x{}.png".format(size,size)
+        output_filename = os.path.join(dirname, filename)
+        command = "inkscape -C -w {w} -h {h} -e \"{}\" \"{}\"".format(output_filename, svg_path, w=size, h=size)
+        inkex.errormsg('<link rel="icon" href="{}" sizes="{w}x{h}">'.format(filename, w=size, h=size))
         p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p.wait()
 
@@ -42,4 +54,4 @@ class PNGExport(inkex.Effect):
 if __name__ == "__main__":
     e = PNGExport()
     e.affect()
-    e.close()
+    #e.close()
---
+++ install_macos.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -e
+cp export_web.inx export_web.py /Applications/Inkscape.app/Contents/Resources/share/inkscape/extensions/
